4825e51b6feb67eded2f631a0d6a38bd02644bd3
racampita <56911151+racampita@users.noreply.github.com>
2021-05-17 22:57:28 +0800

    Initial commit

---
+++ Effects/GrassEffect.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+onready var animatedSprite = $AnimatedSprite
+
+func _ready() -> void:
+	animatedSprite.frame = 0
+	animatedSprite.play("Animate")
+
+
+
+func _on_AnimatedSprite_animation_finished() -> void:
+	queue_free()
